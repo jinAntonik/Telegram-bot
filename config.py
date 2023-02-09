b273c2a7aacf05fac456dc8292949bd07432d246
Anton Alekseev <93080874+jinAntonik@users.noreply.github.com>
2023-02-09 03:29:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-TOKEN = "5987891710:AAHkQ9aBEtf78pzlw4oPD1YqjmEgVbNt2k4"
-TOKEN_SUPPORT = "5943087967:AAFKWsriBSmjb88bNPydHHDyeUdaHdFQfKQ"
-admin_id = [536070296]  # Anton
-users = [448332688]  # Natasha
+TOKEN = "*****************************************"
+admin_id = [*********, *********]
+users = [**********, *********, **********, **********]
 new_csv_file = True  # if True - data is saved in separate file
